b2e3e0c3e951b261f1b4c29ec086eea5525b3ac5
Abril31 <brivaldi0416@gmail.com>
2023-09-05 20:29:28 -0400

    HWE1 web server

--- 02-WebServer/homework/01 - Exercises/server.js
+++ 02-WebServer/homework/01 - Exercises/server.js
@@ -2,9 +2,57 @@ var fs = require("fs");
 var http = require("http");
 /* ⚠️ NO MODIFICAR NADA POR ENCIMA DE ESTA LÍNEA ⚠️ */
 /* AQUÍ DEBAJO PUEDES ESCRIBIR LA CONSTANTE DEL PUERTO */
-const PORT = null;
+const PORT = 3001;
 
 /* ⚠️ LA LÍNEA SIGUIENTE TIENE QUE QUEDAR COMO ESTÁ PARA PODER EXPORTAR EL SERVIDOR ⚠️ */
 module.exports =
   /* AQUÍ DEBAJO YA PUEDES ESCRIBIR TÚ CÓDIGO REEMPLAZANDO EL VALOR DE NULL POR EL SERVIDOR */
-  null;
+  3001;
+
+const server = http
+  .createServer((request, response) => {
+    console.log(`Server raised in port ${PORT}`);
+    const { url } = request;
+    if (url === "/api") {
+      fs.readFile("utils/dogsData.json", (err, data) => {
+        if (err) {
+          response.writeHead(404, {
+            "Content-Type": "text/plain",
+          });
+          return response.end("json not found");
+        } else {
+          response.writeHead(200, {
+            "Content-Type": "application/json",
+          });
+          return response.end(data);
+        }
+      });
+      return;
+    }
+
+    if (url === "/allDogs") {
+      fs.readFile("utils/allDogs.html", "UTF8", (err, data) => {
+        if (err) {
+          response.writeHead(404, {
+            "Content-Type": "text/plain",
+          });
+          return response.end("html not found");
+        } else {
+          response.writeHead(200, {
+            "Content-Type": "text/html",
+          });
+          response.end(data);
+        }
+      });
+      return;
+      }
+      
+      response.writeHead(404, {
+        "Content-Type": "text/plain",
+      });
+      response.end("Route not found");
+    })
+  
+  .listen(PORT, "localhost");
+
+module.exports = server;
